96070b2de78cc27b6aaadbc0da0e0e640c659b1b
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-08-11 11:40:15 -0300

    create module for home

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:modular_example/app/modules/home/home_module.dart';
 
 class AppModule extends Module {
   // Provide a list of dependencies to inject into your project
@@ -7,5 +8,5 @@ class AppModule extends Module {
 
   // Provide all the routes for your module
   @override
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes => [ModuleRoute('/home', module: HomeModule())];
 }
--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -7,12 +7,8 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Modular"),
-        ),
-      ),
+      initialRoute: '/home',
+      debugShowCheckedModeBanner: false,
     ).modular();
   }
 }
