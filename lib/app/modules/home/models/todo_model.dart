0056cb975442fc1413578dbe299aa35a2c54b7b5
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-08-11 17:16:39 -0300

    config firebase in the project

--- lib/app/modules/home/models/todo_model.dart
+++ lib/app/modules/home/models/todo_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class TodoModel {
   String title;
   bool check;
@@ -6,4 +8,26 @@ class TodoModel {
     required this.title,
     required this.check,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'check': check,
+    };
+  }
+
+  factory TodoModel.fromMap(Map<String, dynamic> map) {
+    return TodoModel(
+      title: map['title'],
+      check: map['check'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TodoModel.fromJson(String source) =>
+      TodoModel.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'TodoModel(title: $title, check: $check)';
 }
--- lib/app/modules/home/repository/todo_repository.dart
+++ lib/app/modules/home/repository/todo_repository.dart
@@ -1,9 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:modular_example/app/modules/home/models/todo_model.dart';
 import 'package:modular_example/app/modules/home/repository/todo_repository_interface.dart';
 
 class TodoRepository implements ITodoRepository {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Stream<List<TodoModel>> getTodos() {
-    throw UnimplementedError();
+    return _firestore.collection('todo').snapshots().map((query) {
+      return query.docs.map((doc) {
+        return TodoModel.fromMap(doc.data());
+      }).toList();
+    });
   }
 }
