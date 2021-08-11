8ebe4badaf62b6c7bb08d5b6b61ee57469064a33
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-08-11 11:17:24 -0300

    initial commit

---
+++ lib/app/app_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  // Provide a list of dependencies to inject into your project
+  @override
+  List<Bind> get binds => [];
+
+  // Provide all the routes for your module
+  @override
+  List<ModularRoute> get routes => [];
+}
